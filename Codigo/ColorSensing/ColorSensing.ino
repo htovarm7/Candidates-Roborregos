1c875ecfb13dac2a1b3c1126405e7926282922e3
Alejandro Guajardo <alexguajardoc0902@gmail.com>
2024-10-22 14:07:08 -0600

    Lógica ahora en función separada

--- Codigo/ColorSensing/ColorSensing.ino
+++ Codigo/ColorSensing/ColorSensing.ino
@@ -4,8 +4,7 @@
 
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_1X);
 
-void setup()
-{
+void setup(){
   Serial.begin(9600);
 
   if (!tcs.begin())
@@ -15,57 +14,59 @@ void setup()
   }
 }
 
-void loop()
-{
-  uint16_t clear, red, green, blue;
-
-  tcs.setInterrupt(false);
-  delay(60); // Takes 50ms to capture the color
-  tcs.getRawData(&red, &green, &blue, &clear);
-  tcs.setInterrupt(true);
-
-  // Make rgb measurement relative
-  uint32_t sum = clear;
-  float r, g, b;
-  r = red; r /= sum;
-  g = green; g /= sum;
-  b = blue; b /= sum;
-
-  // Scale rgb to bytes
-  r *= 256; g *= 256; b *= 256;
-
-  // Convert to hue, saturation, value
-  double hue, saturation, value;
-  ColorConverter::RgbToHsv(static_cast<uint8_t>(r), static_cast<uint8_t>(g), static_cast<uint8_t>(b), hue, saturation, value);
-
-  // Show color name
-  printColorName(hue * 360);
-
-  delay(1000);
+void loop(){
+    string color = getColor();
+    delay(1000);
 }
 
-void printColorName(double hue)
-{
+string getColor() {
+    // Código basado mayormente en https://www.luisllamas.es/en/arduino-rgb-color-sensor-tcs34725/
+    uint16_t clear, red, green, blue;
+
+    tcs.setInterrupt(false);
+    delay(60); // Takes 50ms to capture the color
+    tcs.getRawData(&red, &green, &blue, &clear);
+    tcs.setInterrupt(true);
+
+    // Make rgb measurement relative
+    uint32_t sum = clear;
+    float r, g, b;
+    r = red; r /= sum;
+    g = green; g /= sum;
+    b = blue; b /= sum;
+
+    // Scale rgb to bytes
+    r *= 256; g *= 256; b *= 256;
+
+    // Convert to hue, saturation, value
+    double hue, saturation, value;
+    ColorConverter::RgbToHsv(static_cast<uint8_t>(r), static_cast<uint8_t>(g), static_cast<uint8_t>(b), hue, saturation, value);
+
+    // Show color name
+    return commonColor(hue * 360);
+}
+
+string commonColor(double hue){
+    // Valores encontrados después de probar.
     if (hue > 30 && hue < 105) {
-        Serial.println("Yellow");
+        return "Yellow";
     }
     else if (hue > 222 && hue < 265) {
-        Serial.println("Purple");
+        return "Purple";
     }
     else if (hue > 195 && hue < 222) {
-        Serial.println("Blue");
+        return "Blue";
     }
     else if (hue > 300 && hue < 350) {
-        Serial.println("Pink");
+        return "Pink";
     }
     else if (hue > 0 && hue < 25) {
-        Serial.println("Red");
+        return "Red";
     }
     else if (hue > 120 && hue < 160) {
-        Serial.println("Green");
+        return "Green";
     }
     else {
-        Serial.println("None of the above");
+        return "None of the above";
     }
-    Serial.println(hue);
 }
