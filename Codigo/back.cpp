5c646cfc649cde0ce245b412275933c097be6481
Alejandro Guajardo <alexguajardoc0902@gmail.com>
2024-10-27 20:12:42 -0600

    Lógica con paredes lista

--- Codigo/back.cpp
+++ Codigo/back.cpp
@@ -4,8 +4,8 @@
 #include <queue>
 #include <map>
 
-#include "Ultrasonico.h"
-#include "ColorSensing.ino" // Is this even a thing...?
+//#include "Ultrasonico.h"
+//#include "ColorSensing.ino" // Is this even a thing...?
 
 using namespace std;
 
@@ -15,25 +15,90 @@ vector<vector<string>> colorMap(3, vector<string> (5, ""));
 // .....
 
 vector<vector<bool>> verticalWalls(3, vector<bool> (4, 0));
-// ####
-// ####
-// ####
+
+void setVerticalWalls(){
+    verticalWalls[0][0] = 0;
+    verticalWalls[0][1] = 0;
+    verticalWalls[0][2] = 0;
+    verticalWalls[0][3] = 0;
+
+    verticalWalls[1][0] = 0;
+    verticalWalls[1][1] = 0;
+    verticalWalls[1][2] = 0;
+    verticalWalls[1][3] = 0;
+
+    verticalWalls[2][0] = 1;
+    verticalWalls[2][1] = 0;
+    verticalWalls[2][2] = 0;
+    verticalWalls[2][3] = 0;
+    // These values transfer to the following situation:
+    // ....
+    // ....
+    // #...
+}
 
 vector<vector<bool>> horizontalWalls(2, vector<bool> (5, 0));
-// #####
-// #####
+
+void setHorizontalWalls(){
+    horizontalWalls[0][0] = 0;
+    horizontalWalls[0][1] = 1;
+    horizontalWalls[0][2] = 1;
+    horizontalWalls[0][3] = 1;
+    horizontalWalls[0][4] = 1;
+
+    horizontalWalls[1][0] = 0;
+    horizontalWalls[1][1] = 1;
+    horizontalWalls[1][2] = 1;
+    horizontalWalls[1][3] = 0;
+    horizontalWalls[1][4] = 0;
+    // These values transfer to the following situation:
+    // .####
+    // .##..
+}
 
 // Set that holds the currently visited cells.
 set<pair<int, int>> visited;
 
 // Vector that maps the four directions —up, left, down, right— respectively.
-vector<pair<int, int>> d = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+vector<pair<int, int>> directions = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
 
 // Adjacency list of the area.
 map<pair<int, int>, set<pair<int, int>>> AL;
 
 // Map containing the ocurrences of each color.
 map<string, int> detectedColors;
+
+bool checkWall(int direction, pair<int, int> currentNode) {
+    // Values based on "directions" vector
+    enum Direction {
+        UP = 0,
+        LEFT = 1,
+        DOWN = 2,
+        RIGHT = 3
+    };
+
+    // Cartesian notation for readbility's sake.
+    int x = currentNode.first;
+    int y = currentNode.second;
+
+    if (direction == UP) {
+        // Check horizontal wall at (x - 1, y).
+        return horizontalWalls[x - 1][y];
+    }
+    else if (direction == LEFT) {
+        // Check vertical wall at (x, y - 1).
+        return verticalWalls[x][y - 1];
+    }
+    else if (direction == DOWN) {
+        // Check horizontal wall at (x, y).
+        return horizontalWalls[x][y];
+    }
+    else { // if (direction == RIGHT)
+        // Check vertical wall at (x, y).
+        return verticalWalls[x][y];
+    }
+    
+}
 
 void dfs(pair<int, int> node) {
     visited.insert(node);
@@ -53,7 +118,10 @@ void dfs(pair<int, int> node) {
     //     adelante(15);
     // }
 
-    for (auto [dx, dy] : d) {
+    for (int i = 0; i < 4; i++) {
+        int dx = directions[i].first;
+        int dy = directions[i].second;
+
         int nx = node.first + dx;
         int ny = node.second + dy;
 
@@ -64,7 +132,12 @@ void dfs(pair<int, int> node) {
         // girar(90) o algo así
 
         // If there's a wall, skip.
+        // Physical:
         // if (Ultrasonico::medirDistancia() < 10) continue;
+        // Virutal (test):
+        bool wall = checkWall(i, node);
+
+        if (wall) continue;
 
         // If there's no wall, update adjacency list. 
         AL[node].insert({nx, ny});
@@ -113,6 +186,10 @@ map<pair<int, int>, pair<int, int>> bfs(pair<int, int> start) {
 
 
 int main() {
+    // Testing:
+    setVerticalWalls();
+    setHorizontalWalls();
+
     // DFS to reach (0,0), then BFS to know the path to reach the ones that are missing...
         // Counter cases? 
     
