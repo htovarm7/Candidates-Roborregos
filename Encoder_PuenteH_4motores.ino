b3218dcffa4a8bf9c74db14db7b9f35a152fc83a
Josue <josue.tijerina@outlook.com>
2024-10-28 23:59:35 -0600

    Código 4 motores mejorado

--- Encoder_PuenteH_4motores.ino
+++ Encoder_PuenteH_4motores.ino
@@ -1,72 +1,138 @@
-const int ENC_A = 6;
-const int ENC_B = 5;
+// Pines para los encoders de cada motor
+const int ENC_A1 = 6;
+const int ENC_B1 = 5;
+const int ENC_A2 = 7;
+const int ENC_B2 = 8;
+const int ENC_A3 = 9;
+const int ENC_B3 = 10;
+const int ENC_A4 = 11;
+const int ENC_B4 = 12;
 
-// Motor amarillo
-const int IN1 = 2;
-const int IN2 = 4;
-const int ENA = 3;
+// Pines de control para el Motor 1
+const int IN1_M1 = 2;
+const int IN2_M1 = 4;
+const int ENA_M1 = 3;
 
-volatile int pulseCount = 0;
+// Pines de control para el Motor 2
+const int IN1_M2 = 13;
+const int IN2_M2 = 14;
+const int ENA_M2 = 15;
+
+// Pines de control para el Motor 3
+const int IN1_M3 = 16;
+const int IN2_M3 = 17;
+const int ENA_M3 = 18;
+
+// Pines de control para el Motor 4
+const int IN1_M4 = 19;
+const int IN2_M4 = 20;
+const int ENA_M4 = 21;
+
+volatile int pulseCount1 = 0;
+volatile int pulseCount2 = 0;
+volatile int pulseCount3 = 0;
+volatile int pulseCount4 = 0;
+
 unsigned long lastTime = 0;
 const unsigned long interval = 1000; // Intervalo de tiempo en milisegundos
 
 void setup() {
   Serial.begin(9600);
-  pinMode(ENC_A, INPUT);
-  pinMode(ENC_B, INPUT);
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(ENA, OUTPUT);
+  
+  // Configurar pines de encoder como entrada
+  pinMode(ENC_A1, INPUT);
+  pinMode(ENC_B1, INPUT);
+  pinMode(ENC_A2, INPUT);
+  pinMode(ENC_B2, INPUT);
+  pinMode(ENC_A3, INPUT);
+  pinMode(ENC_B3, INPUT);
+  pinMode(ENC_A4, INPUT);
+  pinMode(ENC_B4, INPUT);
 
-  attachInterrupt(digitalPinToInterrupt(ENC_A), countPulse, RISING);
+  // Configurar pines de los motores como salida
+  pinMode(IN1_M1, OUTPUT);
+  pinMode(IN2_M1, OUTPUT);
+  pinMode(ENA_M1, OUTPUT);
+
+  pinMode(IN1_M2, OUTPUT);
+  pinMode(IN2_M2, OUTPUT);
+  pinMode(ENA_M2, OUTPUT);
+
+  pinMode(IN1_M3, OUTPUT);
+  pinMode(IN2_M3, OUTPUT);
+  pinMode(ENA_M3, OUTPUT);
+
+  pinMode(IN1_M4, OUTPUT);
+  pinMode(IN2_M4, OUTPUT);
+  pinMode(ENA_M4, OUTPUT);
+
+  // Interrupciones para contar pulsos de encoders
+  attachInterrupt(digitalPinToInterrupt(ENC_A1), countPulse1, RISING);
+  attachInterrupt(digitalPinToInterrupt(ENC_A2), countPulse2, RISING);
+  attachInterrupt(digitalPinToInterrupt(ENC_A3), countPulse3, RISING);
+  attachInterrupt(digitalPinToInterrupt(ENC_A4), countPulse4, RISING);
 }
 
 void loop() {
   unsigned long currentTime = millis();
   if (currentTime - lastTime >= interval) {
     lastTime = currentTime;
-    float rpm = (pulseCount / 20.0) * 60.0; // Suponiendo 20 pulsos por revolución
-    Serial.print("RPM: ");
-    Serial.println(rpm);
-    pulseCount = 0;
+
+    // Cálculo de RPM de cada motor
+    float rpm1 = (pulseCount1 / 20.0) * 60.0;
+    float rpm2 = (pulseCount2 / 20.0) * 60.0;
+    float rpm3 = (pulseCount3 / 20.0) * 60.0;
+    float rpm4 = (pulseCount4 / 20.0) * 60.0;
+
+    Serial.print("RPM Motor 1: "); Serial.println(rpm1);
+    Serial.print("RPM Motor 2: "); Serial.println(rpm2);
+    Serial.print("RPM Motor 3: "); Serial.println(rpm3);
+    Serial.print("RPM Motor 4: "); Serial.println(rpm4);
+
+    // Resetear contadores de pulsos
+    pulseCount1 = 0;
+    pulseCount2 = 0;
+    pulseCount3 = 0;
+    pulseCount4 = 0;
   }
-  motor();
+  
+  // Controlar los motores
+  motorControl(1, 30); // Motor 1 a velocidad 30
+  motorControl(2, 30); // Motor 2 a velocidad 30
+  motorControl(3, 30); // Motor 3 a velocidad 30
+  motorControl(4, 30); // Motor 4 a velocidad 30
 }
 
-void countPulse() {
-  pulseCount++;
-}
+// Función para contar pulsos de cada encoder
+void countPulse1() { pulseCount1++; }
+void countPulse2() { pulseCount2++; }
+void countPulse3() { pulseCount3++; }
+void countPulse4() { pulseCount4++; }
 
-void motor() {
-  Serial.println("Motor detenido");
-  digitalWrite(IN1, LOW); // Detener motor
-  digitalWrite(IN2, LOW);
-  analogWrite(ENA, 0);    // Sin velocidad
-  delay(2000);
-
-// velocidad mínimo sin peso: 30
-  Serial.println("Motor a velocidad media");
-  digitalWrite(IN1, HIGH); // Sentido del motor
-  digitalWrite(IN2, LOW);
-  analogWrite(ENA, 30);   // Velocidad media (50%)
-  delay(2000);
-
-  // Serial.println("Motor a velocidad máxima");
-  // digitalWrite(IN1, HIGH); // Sentido del motor
-  // digitalWrite(IN2, LOW);
-  // analogWrite(ENA, 154);   // Velocidad máxima (100%)
-  // delay(4000);
-
-  // Serial.println("Motor detenido");
-  // digitalWrite(IN1, LOW); // Detener motor
-  // digitalWrite(IN2, LOW);
-  // analogWrite(ENA, 0);    // Sin velocidad
-  // delay(2000);
-
-// Cambio de dirección
-//  Serial.println("Motor cambiando de dirección");
-//  digitalWrite(IN1, LOW);  // Cambiar sentido del motor
-//  digitalWrite(IN2, HIGH);
-//  analogWrite(ENA, 60);   // Mantener velocidad máxima
-//  delay(2000);
+// Función de control para los motores
+void motorControl(int motor, int velocidad) {
+  int in1, in2, ena;
+  
+  // Seleccionar los pines del motor correspondiente
+  switch (motor) {
+    case 1:
+      in1 = IN1_M1; in2 = IN2_M1; ena = ENA_M1;
+      break;
+    case 2:
+      in1 = IN1_M2; in2 = IN2_M2; ena = ENA_M2;
+      break;
+    case 3:
+      in1 = IN1_M3; in2 = IN2_M3; ena = ENA_M3;
+      break;
+    case 4:
+      in1 = IN1_M4; in2 = IN2_M4; ena = ENA_M4;
+      break;
+    default:
+      return;
+  }
+  
+  // Control de dirección y velocidad
+  digitalWrite(in1, HIGH);  // Sentido de rotación
+  digitalWrite(in2, LOW);   // Sentido de rotación
+  analogWrite(ena, velocidad); // Velocidad
 }
