3a6060ec7a3d131e4a0915e7c02cc16ed7136f51
Héctor Tovar <h.tovarm07@gmail.com>
2024-11-01 12:22:06 -0600

    update

--- Test/Prueba_Sensores/Sensores.ino
+++ Test/Prueba_Sensores/Sensores.ino
@@ -387,6 +387,57 @@ void calculateRPM() {
     }
 }
 
+void RGB() {
+  // Yellow
+  Serial.println("Yellow");
+  analogWrite(R, 255);
+  analogWrite(G, 255);
+  analogWrite(B, 0);
+  delay(2000);
+
+  // Black
+  Serial.println("Black");
+  analogWrite(R, 0);
+  analogWrite(G, 0);
+  analogWrite(B, 0);
+  delay(2000);
+
+  // Purple
+  Serial.println("Purple");
+  analogWrite(R, 31);
+  analogWrite(G, 0);
+  analogWrite(B, 56);
+  delay(2000);
+  
+  // Blue
+  Serial.println("Blue");
+  analogWrite(R, 0);
+  analogWrite(G, 0);
+  analogWrite(B, 255);
+  delay(2000);
+
+  // Pink
+  Serial.println("Pink");
+  analogWrite(R, 255);
+  analogWrite(G, 0);
+  analogWrite(B, 200);
+  delay(2000);
+
+  // Red
+  Serial.println("Red");
+  analogWrite(R, 255);
+  analogWrite(G, 0);
+  analogWrite(B, 0);
+  delay(2000);
+
+  // Green
+  Serial.println("Green");
+  analogWrite(R, 0);
+  analogWrite(G, 255);
+  analogWrite(B, 0);
+  delay(2000);
+}
+
 void setup(){
     // Ultrasonico Izquierdo
     pinMode(leftEcho, INPUT);
@@ -486,7 +537,6 @@ void loop(){
     delay(1000);
 
     calculateRPM();
-
-
+    RGB();
 
 }
