6758dcef0dc1f9dbf3d022163037ad83f1711338
Héctor Tovar <h.tovarm07@gmail.com>
2024-11-02 03:47:18 -0600

    update

--- Test/ColorSensing/RGB.ino
+++ Test/ColorSensing/RGB.ino
@@ -1,65 +1,65 @@
-const int R = 10;
-const int G = 11;
-const int B = 12;
+// const int R = 6;
+// const int G = 8;
+// const int B = 7;
 
-void differentColor() {
-  // Yellow
-  Serial.println("Yellow");
-  analogWrite(R, 255);
-  analogWrite(G, 255);
-  analogWrite(B, 0);
-  delay(2000);
+// void differentColor() {
+//   // Yellow
+//   Serial.println("Yellow");
+//   analogWrite(R, 255);
+//   analogWrite(G, 255);
+//   analogWrite(B, 0);
+//   delay(2000);
 
-  // Black
-  Serial.println("Black");
-  analogWrite(R, 0);
-  analogWrite(G, 0);
-  analogWrite(B, 0);
-  delay(2000);
+//   // Black
+//   Serial.println("Black");
+//   analogWrite(R, 0);
+//   analogWrite(G, 0);
+//   analogWrite(B, 0);
+//   delay(2000);
 
-  // Purple
-  Serial.println("Purple");
-  analogWrite(R, 31);
-  analogWrite(G, 0);
-  analogWrite(B, 56);
-  delay(2000);
+//   // Purple
+//   Serial.println("Purple");
+//   analogWrite(R, 31);
+//   analogWrite(G, 0);
+//   analogWrite(B, 56);
+//   delay(2000);
   
-  // Blue
-  Serial.println("Blue");
-  analogWrite(R, 0);
-  analogWrite(G, 0);
-  analogWrite(B, 255);
-  delay(2000);
+//   // Blue
+//   Serial.println("Blue");
+//   analogWrite(R, 0);
+//   analogWrite(G, 0);
+//   analogWrite(B, 255);
+//   delay(2000);
 
-  // Pink
-  Serial.println("Pink");
-  analogWrite(R, 255);
-  analogWrite(G, 0);
-  analogWrite(B, 200);
-  delay(2000);
+//   // Pink
+//   Serial.println("Pink");
+//   analogWrite(R, 255);
+//   analogWrite(G, 0);
+//   analogWrite(B, 200);
+//   delay(2000);
 
-  // Red
-  Serial.println("Red");
-  analogWrite(R, 255);
-  analogWrite(G, 0);
-  analogWrite(B, 0);
-  delay(2000);
+//   // Red
+//   Serial.println("Red");
+//   analogWrite(R, 255);
+//   analogWrite(G, 0);
+//   analogWrite(B, 0);
+//   delay(2000);
 
-  // Green
-  Serial.println("Green");
-  analogWrite(R, 0);
-  analogWrite(G, 255);
-  analogWrite(B, 0);
-  delay(2000);
-}
+//   // Green
+//   Serial.println("Green");
+//   analogWrite(R, 0);
+//   analogWrite(G, 255);
+//   analogWrite(B, 0);
+//   delay(2000);
+// }
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(R,OUTPUT);
-  pinMode(G,OUTPUT);
-  pinMode(B,OUTPUT);
-}
+// void setup() {
+//   Serial.begin(9600);
+//   pinMode(R,OUTPUT);
+//   pinMode(G,OUTPUT);
+//   pinMode(B,OUTPUT);
+// }
 
-void loop() {
-  differentColor();
-}
+// void loop() {
+//   differentColor();
+// }
